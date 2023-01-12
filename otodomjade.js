401a2ba511669a679a6974daab0dc4a0d28e7f34
Jakub Sowa <sowa.jakub@gmail.com>
2023-01-12 15:52:58 +0100

    web extension learing

---
+++ otodomjade.js
@@ -0,0 +1,19 @@
+
+console.log("script start")
+
+// let script = document.getElementById("__NEXT_DATA__");
+// let json = script.text;
+// let data = JSON.parse(json);
+// console.log("data parsed")
+// console.log(data);
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         var activeTab = tabs[0];
+//         chrome.tabs.create({
+//             url: "https://jakdojade.pl/warszawa/trasa/" + "10",
+//             index: activeTab.index + 1,
+//             active: true
+//         });
+//     });
+// });
