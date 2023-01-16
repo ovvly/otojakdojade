d3257f9f62f3dc329129541b92d8883a6bd1fbd9
Jakub Sowa <sowa.jakub@gmail.com>
2023-01-16 18:16:48 +0100

    jakdojade url params are extracted to vars. Added params for arrival at 9 am

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
 chrome.runtime.onMessage.addListener(function(message, sender, reply) {
-    var lat = message.lat;
-    var long = message.long;
-    var url = `https://jakdojade.pl/warszawa/trasa/?fc=${lat}:${long}&tc=52.2302:21.0114`;
+    var fcLat = message.lat;
+    var fcLong = message.long;
+    var tcLat = 52.2302;
+    var tcLong = 21.0114;
+    var hour = '09:00';
+    var isArrival = 'true'; 
+
+    var url = `https://jakdojade.pl/warszawa/trasa/?fc=${fcLat}:${fcLong}&tc=${tcLat}:${tcLong}&h=${hour}&ia=${isArrival}`;
+    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var activeTab = tabs[0];
         chrome.tabs.create({
