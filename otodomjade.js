5ba9f516a7119ababe3e03152af1b4475e794538
Jakub Sowa <sowa.jakub@gmail.com>
2023-01-16 18:10:31 +0100

    button jakdojade is on top

--- otodomjade.js
+++ otodomjade.js
@@ -4,14 +4,13 @@ var data = JSON.parse(json);
 var adData = data.props.pageProps.adTrackingData;
 var lat = adData.lat;
 var long = adData.long;
-var url = `https://jakdojade.pl/warszawa/trasa/?fc=${lat}:${long}&tc=52.31420522649696:20.959419667518077`;
 
 var button = document.createElement("button");
 var title = document.createTextNode("jakdojade");
 button.appendChild(title);
 
 var next = document.getElementById("__next");
-next.appendChild(button);
+next.firstChild.prepend(button);
 
 button.addEventListener("click", () => {
     chrome.runtime.sendMessage({
